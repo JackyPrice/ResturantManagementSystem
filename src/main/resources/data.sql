fc5003a214b6434b62d9ebc29377b595e26e1322
Jacky Price <jackyprice@rentalcars.com>
2021-02-25 20:05:54 +0000

    committing omitted data.sql file

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO restaurant_management.restaurant_management.reservations(id, first_name, last_name, reservation_time, number_of_guests)
+INSERT INTO reservations(id, first_name, last_name, reservation_time, number_of_guests)
 VALUES ('Jacice10000', 'Jacky', 'Price', '2020-02-24 17:23:30', 3);
